093ab432ea0aa304ae4e970887ef2f4375ce60a2
Adrian Lalusin <77495466+AALAdrian@users.noreply.github.com>
2022-07-27 22:35:10 +0800

    update scripts

--- scripts/signup.js
+++ scripts/signup.js
@@ -4,28 +4,33 @@ form.addEventListener('submit',function(event){
     event.preventDefault();
     console.log("working");
     var firstname = document.getElementById('firstname').value;
-	var student_num = document.getElementById('school_num').value;
-	sessionStorage.setItem('student_num', student_num);
+	var student_num = document.getElementById('school_num').value;	
 	var email = document.getElementById('email').value;
-	var gender = document.getElementById('gender');
-	var gender_value = gender.options[gender.selectedIndex].value;
-	console.log(gender_value);
-	formData = new FormData();
-	formData.append('firstname', firstname);
-	formData.append('student_num', student_num);
-	formData.append('email', email);
-	formData.append('gender', gender_value);
-    var request = new XMLHttpRequest();
-		request.onreadystatechange = function(){
-			if(request.readyState == 4 && request.status == 200)
-			{
+	var checking = validation(email, student_num);
+	if (checking == true) {
+		sessionStorage.setItem('student_num', student_num);
+		var gender = document.getElementById('gender');
+		var gender_value = gender.options[gender.selectedIndex].value;
+		console.log(gender_value);
+		formData = new FormData();
+		formData.append('firstname', firstname);
+		formData.append('student_num', student_num);
+		formData.append('email', email);
+		formData.append('gender', gender_value);
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function () {
+			if (request.readyState == 4 && request.status == 200) {
 				alert(request.responseText);
 			}
 		}
 		request.open('POST', 'signup.php');
 		request.send(formData);
 
-    window.location = "sign2.html";
+		window.location = "sign2.html";
+	}
+	else {
+		document.getElementById('check').innerHTML = "<p>Invalid Input. Existing email or student id</p>"
+    }
 });
 
 
