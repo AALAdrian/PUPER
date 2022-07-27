ca7619f80c104bfc597dd07f7748c01ba0f97581
Adrian Lalusin <77495466+AALAdrian@users.noreply.github.com>
2022-07-27 21:48:08 +0800

    initial

---
+++ scripts/sign2.js
@@ -0,0 +1,24 @@
+var form = document.getElementById('form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log("working");
+
+    var username = document.getElementById('nameInput').value;
+    var password = document.getElementById('signupPass').value;
+    var student_num = sessionStorage.getItem('student_num');
+    formData = new FormData();
+    formData.append('username', username);
+    formData.append('password', password);
+    formData.append('student_num', student_num);
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
+            alert(request.responseText);
+        }
+    }
+    request.open('POST', 'sign2.php');
+    request.send(formData);
+
+    window.location = "sign3.html";
+});
