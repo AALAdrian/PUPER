792f5f6f1573df82468fc484e7a76429ec8c81dd
Amiel Osias Divinagracia <amielosiasddivinagracia@iskolarngbayan.pup.edu.ph>
2022-07-28 11:30:56 +0800

    slight changes in js and html

--- js/GET.js
+++ js/GET.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', (event) => {
     var request = new XMLHttpRequest();
-    request.open('GET', 'scripts/validation.php', true);
+    request.open('GET', 'http://192.168.254.111/cordova/validation.php', true);
     request.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var data = this.responseText;
